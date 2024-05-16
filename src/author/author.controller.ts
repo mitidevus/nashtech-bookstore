d79531a3a45a4ef5d2788e62f330287e8880e77e
Do Minh Tri <mitidevus@gmail.com>
2024-05-16 16:50:59 +0700

    feat: authorization

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtGuard } from 'src/auth/guard';
+import { UserRole } from '@prisma/client';
+import { Roles } from 'src/auth/decorator';
+import { JwtGuard, RolesGuard } from 'src/auth/guard';
 import { AuthorService } from './author.service';
 
 @Controller('authors')
 export class AuthorController {
   constructor(private readonly authorService: AuthorService) {}
 
-  @UseGuards(JwtGuard)
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(UserRole.admin)
   @Get()
   getAuthors() {
     return this.authorService.getAuthors();
