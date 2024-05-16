d6ef6cae3f14d7a724e03ccf764afa002b133752
Do Minh Tri <mitidevus@gmail.com>
2024-05-16 16:40:54 +0700

    feat: authentication

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
 import { AuthorService } from './author.service';
 
 @Controller('authors')
 export class AuthorController {
   constructor(private readonly authorService: AuthorService) {}
 
+  @UseGuards(JwtGuard)
   @Get()
   getAuthors() {
     return this.authorService.getAuthors();
