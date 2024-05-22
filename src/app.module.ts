dc266c3ca4d951a3aca5eee66dd427942d2b1b41
Do Minh Tri <mitidevus@gmail.com>
2024-05-22 15:35:09 +0700

    feat: setup pug

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,15 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PromotionListModule } from './promotion-list/promotion-list.module';
 import { RatingReviewModule } from './rating-review/rating-review.module';
-import { OrderModule } from './order/order.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -32,5 +33,6 @@ import { UserModule } from './user/user.module';
     OrderModule,
     UserModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(resolve('./public'));
+  app.setBaseViewsDir(resolve('./views'));
+  app.setViewEngine('pug');
 
   app.useGlobalPipes(
     new ValidationPipe({
