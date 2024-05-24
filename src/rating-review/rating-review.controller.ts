073b4b22d4a5c3868a8ffcf00eef655c236e0809
Do Minh Tri <mitidevus@gmail.com>
2024-05-24 17:43:43 +0700

    feat: get all rating reviews api

--- src/rating-review/rating-review.controller.ts
+++ src/rating-review/rating-review.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards } from '@nestjs/common';
+import { UserRole } from '@prisma/client';
+import { Roles } from 'src/auth/decorator';
+import { JwtGuard, RolesGuard } from 'src/auth/guard';
+import { RatingReviewsPageOptionsDto } from './dto';
 import { RatingReviewService } from './rating-review.service';
 
-@Controller('rating-reviews')
+@Controller('/api/rating-reviews')
 export class RatingReviewController {
   constructor(private readonly ratingReviewService: RatingReviewService) {}
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(UserRole.admin)
+  @Get()
+  async getRatingReviews(@Body() dto: RatingReviewsPageOptionsDto) {
+    return await this.ratingReviewService.getRatingReviews(dto);
+  }
 }
