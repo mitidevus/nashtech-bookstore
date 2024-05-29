e816d05fac76e45760e6260c04a90593bc828cd7
Do Minh Tri <mitidevus@gmail.com>
2024-05-29 22:48:10 +0700

    feat: get user profile, return more fields

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { GetUser } from './decorator';
 import { LoginRequestDto, SignUpRequestDto } from './dto';
+import { JwtGuard } from './guard';
 
-@Controller('auth')
+@Controller('/api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
@@ -14,5 +16,11 @@ export class AuthController {
   @Post('login')
   async basicLogin(@Body() dto: LoginRequestDto) {
     return await this.authService.login(dto);
+  }
+
+  @Get('me')
+  @UseGuards(JwtGuard)
+  async getPofile(@GetUser('sub') userId: string) {
+    return await this.authService.getProfile(userId);
   }
 }
